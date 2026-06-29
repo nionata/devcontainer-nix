2feef56568f3975e14e7233e2a8c40e2743bbe91
Nick Ionata <n.ionata129@gmail.com>
2026-06-29 04:04:31 +0000

    Update devcontainer and output legacyPackages

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs }: {
+    legacyPackages.aarch64-linux = nixpkgs.legacyPackages.aarch64-linux;
     packages.aarch64-linux.hello = nixpkgs.legacyPackages.aarch64-linux.hello;
   };
 }
